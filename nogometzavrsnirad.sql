0ffa55f042cc026cf60240c083ae7972c5d95ba5
Luka Kordić <lkordic234@gmail.com>
2024-11-23 10:37:55 +0100

    popravljen zavrsni

--- nogometzavrsnirad.sql
+++ nogometzavrsnirad.sql
@@ -11,21 +11,26 @@ go
 use nogomet;
 go
 
+create table trener(
+sifra int not null primary key identity (1,1),
+ime varchar (20) not null,
+prezime varchar (20) not null,
+klub int not null references klub(sifra),
+iskustvo int not null,
+
 create table klub(
 sifra int not null primary key identity(1,1),
 naziv varchar (30) not null,
 osnovan varchar (40) not null,
-stadion int not null,
-trener int not null,
-drzava int not null,
-liga int not null,
+stadion Varchar (50) not null,
+drzava varchar (50) not null,
+liga varchar (50) not null,
 );
 
 create table utakmica(
 sifra int not null primary key identity(1,1),
 datum datetime not null,
 vrijeme time not null,
-stadion varchar (50) not null,
 domaci_klub int  not null references klub(sifra),
 gostojuci_klub int  not null references klub(sifra)
 );
@@ -39,38 +44,6 @@ klub int not null references klub(sifra),
 oib char (11)
 );
 
-create table trener(
-sifra int not null primary key identity (1,1),
-ime varchar (20) not null,
-prezime varchar (20) not null,
-klub int not null references klub(sifra),
-iskustvo int not null,
-);
-
-
-insert into klub (sifra,naziv,osnovan,stadion,trener,drzava,liga)
-values (1,'Barcelona','1899',1,1,1,1);
-
-insert into klub (sifra,naziv,osnovan,stadion,trener,drzava,liga)
-values (2,'Real Madrid',1902,1,1,1,1);
-
-insert into klub (sifra,naziv,osnovan,stadion,trener,drzava,liga)
-values (3,'Chelsea',1905,1,1,1,1);
-
-insert into klub (sifra,naziv,osnovan,stadion,trener,drzava,liga)
-values (4,'Juventus',1897,1,1,1,1);
-
-insert into klub (sifra,naziv,osnovan,stadion,trener,drzava,liga)
-values (5,'Manchester City',1880,1,1,1,1);
-
-insert into klub (sifra,naziv,osnovan,stadion,trener,drzava,liga)
-values (6,'Manchester United',1902,1,1,1,1);
-
-select * from klub;
-
-
-insert into utakmica (datum,vrijeme,stadion,domaci_klub,gostojuci_klub )
-values ('20-11-2024','16;00','Old Trafford',1,3);
 
 
 
@@ -78,5 +51,10 @@ values ('20-11-2024','16;00','Old Trafford',1,3);
 
 
 
-select * from utakmica;
+
+
+
+
+
+
 
