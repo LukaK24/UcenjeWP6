fe42368df3d86fd4657182b4d2ca3f18d7228750
Luka kordić <lkordic234@gmail.com>
2024-11-27 18:32:38 +0100

    nogomet zavrsni rad

--- nogometzavrsnirad.sql
+++ nogometzavrsnirad.sql
@@ -11,85 +11,85 @@
 --use nogomet;
 --go
 
-create table trener(
-sifra int not null primary key identity (1,1),
-ime varchar (20) not null,
-prezime varchar (20) not null,
-klub varchar (50) not null references klub (sifra),
-iskustvo int not null
-);
-
 create table klub(
 sifra int not null primary key identity(1,1),
 naziv varchar (30) not null,
-osnovan varchar (40) not null,
+osnovan int not null,
 stadion Varchar (50) not null,
 drzava varchar (50) not null,
 liga varchar (50) not null
 );
 
-create table utakmica(
+
+create table trener(
+sifra int not null primary key identity (1,1),
+ime varchar (20) not null,
+prezime varchar (20) not null,
+klub int  not null references klub (sifra),
+iskustvo int not null
+);
+
+create table utakmice(
 sifra int not null primary key identity(1,1),
 datum datetime not null,
-vrijeme time not null,
-domaci_klub varchar (40)  not null references klub(sifra),
-gostojuci_klub varchar (40)  not null references klub(sifra)
+vrijeme time,
+domaci_klub int  not null references klub(sifra),
+gostojuci_klub int  not null references klub(sifra)
 );
+
 
 create table igrac(
 sifra int not null primary key identity (1,1),
 ime varchar (40) not null,
 prezime varchar (30) not null,
-pozicija varchar (30) not null,
-klub varchar (50) not null references klub(sifra),
+pozicija varchar (50) not null,
+klub int not null references klub(sifra),
 oib char (11)
 );
 
 
-insert into trener (ime,prezime,klub,iskustvo )
-values 
-('Carlo','Anchelotti','Real Madrid',29),
-('Pep','Guardiola','Manchester City',17),
-('Hansi','Flick','Barcelona',18),
-('Xavi','Alonso','Bayern Leverkusen',6);
-
-select * from trener;
 
 insert into klub (naziv,osnovan,stadion,drzava,liga )
 values 
-('Real Madrid',1902,'Santiago Bernabéu','Španjolska','LaLiga'),
-('Manchester City',1880,'Etihad','Engleska','Premier League'),
-('Barcelona',1899,'Spotify Camp','Španjolska','LaLiga'),
-('Bayern Leverkusen',1904,'BayArena','Njemačka','Bundesliga');
+('1',1902,'Santiago Bernabéu','Španjolska','LaLiga'),
+('2',1880,'Etihad','Engleska','Premier League'),
+('3',1899,'Spotify Camp','Španjolska','LaLiga'),
+('4',1904,'BayArena','Njemačka','Bundesliga');
 
 select * from klub
 
--- Invalid object name 'utakimca'.
-insert into utakimca (datum,vrijeme,domaci_klub,gostojuci_klub)
+insert into trener (ime,prezime,klub,iskustvo )
 values 
-('20-05-2024','16:00','Real Madrid','Barcelona'),
-('14-06-2024','19:00','Manchester City','Bayern Leverkusen'),
-('24-04-2024','18:00','Barcelona','Bayer Leverkusen'),
-('25-05-2024','17:00','Manchester City','Bayer Leverkusen'),
-('23-03-2024','19:00','Barcelona','Manchester City'),
-('01-04-2024','18:00','Real Madrid','Bayer Leverkusen'),
-('05-03-2024','18:00','Real Madrid','Barcelona');
+('Carlo','Anchelotti','1',29), 
+('Pep','Guardiola','2',17),
+('Hansi','Flick','3',18),
+('Xavi','Alonso','4',6);
 
-select * from utakmica
-
-drop table utakmica;
-
---Invalid object name 'igrac'.
-insert into igrac (ime,prezime,pozicija,klub,oib)
-values
-('Federico','Valverde','obrana','Real Madrid',08057077736),
-('Luka','Modrić','veza','Real Madrid',07837540514),
-('Mateo','Kovačić','veza','Manchester City',26981510319),
-('Joško','Gvardiol','Manchester City','obrana',44021291244),
-('Dani','Olmo','Barcelona','veza',44337232806),
-('Lamine','Yamal','Barcelona','napad',25169325706),
-('Jeremine','Frimpong','Bayern Leverkusen','obrana',99598162856),
-('Florian','Wirtz','Bayern Leverkusen','veza',38124701663);
+select * from trener;
 
 
-drop table igrac;
+
+insert into utakmice (datum,domaci_klub,gostojuci_klub)
+values 
+('2024-05-20 18:00','1','3'),  
+('2024-06-14 16:00','2','4'),
+('2024-04-20 19:00','3','4'),
+('2024-05-25 16:00','2','4'),
+('2024-03-23 18:00','3','2'),
+('2024-04-01 20:00', '1','4'),
+('2024-03-05 18:00','1','3');
+
+
+
+INSERT INTO igrac (ime, prezime, pozicija, klub, oib)
+VALUES
+('Federico', 'Valverde', 'RB', 1, '08057077736'),
+('Luka', 'Modrić', 'CM', 1, '07837540514'),
+('Mateo', 'Kovačić', 'CM', 2, '26981510319'),
+('Joško', 'Gvardiol', 'CB', 2, '44021291244'),
+('Dani', 'Olmo', 'CAM', 3, '44337232806'),
+('Lamine', 'Yamal', 'RW', 3, '25169325706'),
+('Jeremine', 'Frimpong', 'RB', 4, '99598162856'),
+('Florian', 'Wirtz', 'CAM', 4, '38124701663');
+
+
