9643e8ec16538fb9686bb5343375b348929d7ab4
LukaK24 <lkordic234@gmail.com>
2024-12-04 16:53:03 +0100

    izjednjačavanje

---
+++ CSHARP/Ucenje/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Ucenje;
+
+E01UlazIzlaz.Izvedi();
+
+// komentar jedna linija
+
+/*
+
+Komentar više linija 
+
+
+ */
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Pozdrav Svijetu ČŠĆĐŽ čšćđž");
+//Console.WriteLine("Hello Osijek");
