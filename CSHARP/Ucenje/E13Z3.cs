b78a6282cf38546799411b0448a650a8cf570302
LukaK24 <lkordic234@gmail.com>
2024-12-18 18:56:37 +0100

    vjezba

---
+++ CSHARP/Ucenje/E13Z3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E13Z3
+    {
+        public static void Izvedi() 
+        {
+            string ime = E12Metode.UcitajString("Unesi ime: ");
+            string prezime = E12Metode.UcitajString("Unesi prezime: ");
+            Console.WriteLine("{0} {1}", prezime,ime);
+          
+           
+
+        }
+    }
+}
