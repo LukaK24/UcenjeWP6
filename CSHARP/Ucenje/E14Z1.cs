df2ba8b25b8db945da711d98527a928544b59065
LukaK24 <lkordic234@gmail.com>
2024-12-20 18:53:53 +0100

    vjezba

--- CSHARP/Ucenje/E14Z1.cs
+++ CSHARP/Ucenje/E14Z1.cs
@@ -18,17 +18,26 @@ namespace Ucenje
         }
         public static void Izbornik() 
         {
+            string[] programi = {
+                "Parnost broja",
+                "Tablica množenja",
+                "Jedinična vrijednosr",
+                "Broj znakova naziva mjesta"
+
+            };
+
             Console.WriteLine();
             Console.WriteLine("IZBORNIK");
-            Console.WriteLine("1. parnost broja");
-            Console.WriteLine("2. Tablica množenja");
-            Console.WriteLine("0. izlaz iz programa");
-            OdabirOpcijeIzbornika();
+           for (int i = 0; i < programi.Length; i++) 
+            {
+                Console.WriteLine( "{0}.{1} ", i+1, programi[i]);
+            }
+            OdabirOpcijeIzbornika(programi.Length);
         }
 
-        private static void OdabirOpcijeIzbornika()
+        private static void OdabirOpcijeIzbornika(int programa)
         {
-            switch(E12Metode.UcitajCijeliBroj("Odaberi stavku izbornika: ",0,2)) 
+            switch(E12Metode.UcitajCijeliBroj("Odaberi stavku izbornika: ",0,programa)) 
             {
                 case 0:
                     break;
@@ -40,12 +49,48 @@ namespace Ucenje
                     TablicaMnoženja();
                     Izbornik();
                     break;
+                case 3:
+                    JediničnaVrijednost();
+                    Izbornik();
+                    break;
+                case 4:
+                    BrojZnakovaNazivaMjesta();
+                    Izbornik();
+                    break;
             }
+        }
+
+        private static void BrojZnakovaNazivaMjesta()
+        {
+            NaslovPrograma("Za uneseni naziv mjesta ispisuje koliko ima znakova");
+            Console.WriteLine(E12Metode.UcitajString("Unesi ime grada: ").Length);
+        }
+
+        
+        
+
+        private static void JediničnaVrijednost()
+        {
+            NaslovPrograma("Jedinična vrijednost");
+            int broj = E12Metode.UcitajCijeliBroj("Unesi broj između 20 i 50: ", 20, 50);
+            Console.WriteLine(broj % 10);
+                
+                
         }
 
         private static void TablicaMnoženja()
         {
             NaslovPrograma("Program koji za dane brojeve redaka i stupaca generira tablicu množenja");
+            int redaka = E12Metode.UcitajCijeliBroj("Unesi broj redaka: ", 2, 10);
+            int stupaca = E12Metode.UcitajCijeliBroj("Unesi broj stupaca: ", 2, 10);
+            for(int i = 1; 1<=redaka; i++) 
+            {
+               for(int j = 1; j <=stupaca; j++) 
+                {
+                    Console.WriteLine("{0,4}",i*j);
+                }
+                Console.WriteLine();
+            }
         }
 
         private static void ParnostBroja()
@@ -66,18 +111,36 @@ namespace Ucenje
 
         private static void NaslovPrograma(string naslov) 
         {
-            Zvjezdice(naslov.Length);
+            NaglasiNaslov(naslov.Length);
             Console.WriteLine(naslov);
-            Zvjezdice(naslov.Length);
+            NaglasiNaslov(naslov.Length);
         }
 
-        private static void Zvjezdice(int komad) 
+        private static void NaglasiNaslov(int komad) 
         {
+           
             for (int i = 0; i < komad; i++)
             {
                 Console.Write("-");
             }
             Console.WriteLine();
         }
+        private static void JediničnaVrijednost(int broj) 
+        {
+            int cijelibroj = E12Metode.UcitajCijeliBroj("Unesi te broj između 20 i 50");
+
+            if (broj >= 20 && broj <= 50) 
+            {
+                int jedinica = broj % 10;
+                Console.WriteLine("Vrijednost jedinice broja {0} je: {1}", broj, jedinica);
+            }
+            else 
+            {
+                Console.WriteLine("Broj nije u rasponud od 20 do 50");
+            }
+            
+            
+        }
+
     }
 }
