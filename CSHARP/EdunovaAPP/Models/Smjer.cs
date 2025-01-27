71233892acc9e7f31fecfc9aab77d407fc2ec32f
LukaK24 <lkordic234@gmail.com>
2025-01-27 18:00:39 +0100

    Priprema za smjer rit

---
+++ CSHARP/EdunovaAPP/Models/Smjer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+namespace EdunovaAPP.Models
+{
+    public class Smjer: Entitet
+    {
+        public string naziv { get; set; } = "";
+        public decimal cijena { get; set; }
+        public datetime izvodseod { get; set; }
+    }
+}
