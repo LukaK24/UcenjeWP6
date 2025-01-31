5263389811571b438f992664a407de1b1814773b
LukaK24 <lkordic234@gmail.com>
2025-01-31 18:48:37 +0100

    Update SmjerController.cs

--- CSHARP/EdunovaAPP/Controllers/SmjerController.cs
+++ CSHARP/EdunovaAPP/Controllers/SmjerController.cs
@@ -34,6 +34,25 @@ namespace EdunovaAPP.Controllers
                 return BadRequest(e);
             }
         }
+        [HttpGet]
+        [Route("{sifra:int}")]
+        public IActionResult GetBysifra(int sifra)
+        {
+            try
+            {
+                var s = _context.Smjerovi.Find(sifra);
+                if (s == null)
+                {
+                    return NotFound();
+                }
+                return Ok(s);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
         [HttpPost]
         public IActionResult Post(Smjer smjer)
         {
@@ -49,6 +68,54 @@ namespace EdunovaAPP.Controllers
             }
         }
 
+        [HttpPut]
+        [Route("{sifra:int}")]
+      
+        public IActionResult Put(int sifra, Smjer smjer)
+        {
+            try
+            {
+                var s = _context.Smjerovi.Find(sifra);
+                if (s == null)
+                {
+                    return NotFound();
+                }
+                s.Naziv = smjer.Naziv;
+                s.CijenaSmjera = smjer.CijenaSmjera;
+                s.IzvodiSeOd = smjer.IzvodiSeOd;
+                s.Vaucer = smjer.Vaucer;
+
+
+                _context.Update(smjer);
+                _context.SaveChanges();
+                return Ok(new {poruka="Uspjeno promijenjeno" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        public IActionResult Delete(int sifra)
+        {
+            try
+            {
+                var s = _context.Smjerovi.Find(sifra);
+                if (s == null)
+                {
+                    return NotFound();
+                }
+                _context.Remove(s);
+                _context.SaveChanges();
+                return Ok(new { poruka = "Uspjeno obrisano" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        
 
     }
 }
